99e3c37cd6717089a330a6a0377536ddb9e50861
John Arena <john.arena.3@gmail.com>
2015-04-23 15:42:57 +0900

    push to io

--- js/trixelator.js
+++ js/trixelator.js
@@ -26,12 +26,21 @@
  
  
  $(document).ready(function() {
+
+//get these tooltips up ins 
+ $(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
+	 
 	 
 //globals for the source
 var imageLoader = document.getElementById('imageLoader');
     imageLoader.addEventListener('change', handleImage, false);
 var inputCanvas = document.getElementById('inputCanvas');
 var i_ctx = inputCanvas.getContext('2d');
+var thumbnailCanvas = document.getElementById('thumbnailCanvas');
+var t_ctx = thumbnailCanvas.getContext('2d');
+var inputImage;
 
 //globals for the options
 var outputSizeMultiplier = 1;
@@ -39,6 +48,8 @@ var cellSize = 20;
 var colorSampleRadius = 1;
 var drawCells = false;
 var sliceCount = 1;
+var bisectDirection = "bis-rnd";
+var noisePattern;
 
 //globals for the output
 var cellSize;
@@ -49,61 +60,85 @@ var o_ctx = outputCanvas.getContext('2d');
 //init sliders
 	var cs_slider = $('#cell-size-slider').slider({
 		formatter: function(value) {
-			return 'Current value: ' + value;
+			return 'Cell Size: ' + value;
 		}
-	}).on("slide", function(slideEvt) {
-		cellSize = (slideEvt.value);
 	});
 
 	var os_slider = $('#output-size-slider').slider({
 		formatter: function(value) {
-			return 'Current value: ' + value;
+			return 'Output Image Scale: x' + value;
 		}
-	}).on("slide", function(slideEvt) {
-		outputSizeMultiplier = (slideEvt.value);
 	});
 
 
 //get image from loader
 function handleImage(e){
     var reader = new FileReader();
+	var canvasExtents = $('#preview').width();
+	
     reader.onload = function(event){
-        var img = new Image();
-        img.onload = function(){
-            inputCanvas.width = img.width;
-            inputCanvas.height = img.height;
-            i_ctx.drawImage(img,0,0);
+        inputImage = new Image();
+        inputImage.onload = function(){
+			
+            inputCanvas.width = thumbnailCanvas.width = inputImage.width;
+            inputCanvas.height = thumbnailCanvas.height = inputImage.height;
+			i_ctx.drawImage(inputImage,0,0);
+			inputCanvas.style.display="none";
+			
+			if (thumbnailCanvas.width > canvasExtents)
+			{
+				//do something
+			}
+			
+		//	t_ctx.scale(0.5,0.5);
+            t_ctx.drawImage(inputImage,0,0);
         }
-        img.src = event.target.result;
+		
+        inputImage.src = event.target.result;
     }
     reader.readAsDataURL(e.target.files[0]);     
 }
 
     $("#render").click(function(){
 		
-		outputCanvas.width = inputCanvas.width;
-		outputCanvas.height = inputCanvas.height;
+		cellSize = $('#cell-size-slider').slider().slider('getValue');
+		outputSizeMultiplier = os_slider;
+		
+		outputCanvas.width = inputImage.width;
+		outputCanvas.height = inputImage.height;
+		
+		bisectDirection = $('input[name="bisect-dir"]:checked').val();
+		
+		//if noise
+		var noiseImg = document.getElementById("noise");
+		noisePattern = o_ctx.createPattern(noiseImg, "repeat");
 				
 		draw_output_cells();
     }); 
 	
-	
-	//define a cell
-	function cell (xStart, yStart) {
-		this.x = xStart;
-		this.y = yStart;
+	//define a coordinate
+	function coord (x, y) {
+		this.x = x;
+		this.y = y;
 	}
 	
+	//define a cell
+	function cell (coords) {
+		this.verts = coords;
+	}
 	
+		
 		//TODO: extend this to other shapes	
 	function draw_output_cells(){
 		var cells=[];
 		var source = i_ctx.getImageData(0, 0, inputCanvas.width, inputCanvas.height);
 
-		$('#render-span').addClass("glyphicon glyphicon-refresh glyphicon-refresh-animate");
+		$('#render-span').addClass("glyphicon glyphicon-refresh");
 		$('#render').text("Working...");		
 		
+		//TODO: cell shape
 		
+		//for square grids
 		//calculate the output grid
 		var grid_length = Math.round(source.width / cellSize);
 		var grid_height = Math.round(source.height / cellSize);		
@@ -112,16 +147,22 @@ function handleImage(e){
 		{
 			for (var y = 0; y < grid_height; y++)
 			{
-				cells.push(new cell(x*cellSize, y*cellSize));			
+					cells.push(
+						new cell([
+							new coord(x * cellSize, y * cellSize),
+							new coord((x + 1) * cellSize, y * cellSize),
+							new coord(x * cellSize, (y - 1) * cellSize),
+							new coord((x + 1) * cellSize, (y - 1) * cellSize)
+						])
+					);					
 			}
 		}
 		
-
 		console.log(cells.length);
 		
 		draw_cells(cells);
 		
-		$('#render-span').removeClass("glyphicon glyphicon-refresh glyphicon-refresh-animate");
+		$('#render-span').removeClass("glyphicon glyphicon-refresh");
 		$('#render').text("Trixelate!");
 
 	}
@@ -143,51 +184,92 @@ function handleImage(e){
 				o_ctx.lineTo(c.x + cellSize, c.y + cellSize);
 				o_ctx.stroke();
 			}
-			
-			//draw triangle bottom
-			o_ctx.fillStyle = getPointColorBottom(c);
-			o_ctx.beginPath();
-			o_ctx.moveTo(c.x, c.y-1);
-			o_ctx.lineTo(c.x + cellSize+1, c.y + cellSize);
-			o_ctx.lineTo(c.x, c.y + cellSize);
-			o_ctx.closePath();
-			o_ctx.fill();
-			
-			//draw triangle top
-			o_ctx.fillStyle = getPointColorTop(c);
-			o_ctx.beginPath();
-			o_ctx.moveTo(c.x, c.y);
-			o_ctx.lineTo(c.x + cellSize, c.y + cellSize);
-			o_ctx.lineTo(c.x + cellSize, c.y);
-			o_ctx.closePath();
-			o_ctx.fill();
-			
+				
+			switch (bisectDirection)
+			{
+				case ("bis-ccw"):
+					//0 -> 3 -> 2
+					drawTriangle(c.verts[0], c.verts[3], c.verts[2])
+					//0 -> 1 -> 3
+					drawTriangle(c.verts[0], c.verts[1], c.verts[3])					
+					break;
+				case ("bis-cw"):
+					//0 -> 1 -> 2
+					drawTriangle(c.verts[0], c.verts[1], c.verts[2])
+					//1 -> 3 -> 2
+					drawTriangle(c.verts[1], c.verts[3], c.verts[2])
+					break;
+				case ("bis-alt"):
+					if (i % 2 === 0)
+					{
+						drawTriangle(c.verts[0], c.verts[3], c.verts[2])
+						drawTriangle(c.verts[0], c.verts[1], c.verts[3])
+					}
+					else
+					{
+						drawTriangle(c.verts[0], c.verts[1], c.verts[2])
+						drawTriangle(c.verts[1], c.verts[3], c.verts[2])						
+					}
+					break;
+				case ("bis-rnd"):
+					if (Math.random() > 0.5)
+					{
+						drawTriangle(c.verts[0], c.verts[3], c.verts[2])
+						drawTriangle(c.verts[0], c.verts[1], c.verts[3])
+					}
+					else
+					{
+						drawTriangle(c.verts[0], c.verts[1], c.verts[2])
+						drawTriangle(c.verts[1], c.verts[3], c.verts[2])						
+					}
+					break;
+				default:
+					alert("ERROR: malformed bisect direction request!")
+					break;				
+			}
+					
 		}
 		
 	}
 	
-	function getPointColorBottom (c)
+	function drawTriangle (a, b, c)
 	{
-		var centroidX = getTriangleCentroidCoordinate(c.x, c.x + cellSize, c.x);
-		var centroidY = getTriangleCentroidCoordinate(c.y, c.y + cellSize, c.y + cellSize);
-		var p = i_ctx.getImageData(centroidX, centroidY, 1, 1).data; 
-		var hex = "#" + ("000000" + rgbToHex(p[0], p[1], p[2])).slice(-6);
-		return hex;
+		
+		o_ctx.fillStyle = o_ctx.strokeStyle = "#" + getPointColor(a,b,c);
+
+		o_ctx.beginPath();
+		o_ctx.lineWidth=1;
+		o_ctx.moveTo(a.x, a.y);
+		o_ctx.lineTo(b.x, b.y);
+		o_ctx.lineTo(c.x, c.y);
+		o_ctx.closePath();
+					
+		o_ctx.fill();
+		o_ctx.stroke();		
+		
+		if ($('#noisy').is(":checked"))
+		{	
+			o_ctx.fillStyle = o_ctx.strokeStyle = noisePattern;
+			o_ctx.fill();
+//			o_ctx.stroke();	
+		}
+		
+		if (!$('#cell-borders').is(":checked"))
+		{
+			o_ctx.stroke();
+		}
 	}
 	
-		function getPointColorTop (c)
+	function getPointColor (a,b,c)
 	{
-		var centroidX = getTriangleCentroidCoordinate(c.x, c.x + cellSize, c.x + cellSize);
-		var centroidY = getTriangleCentroidCoordinate(c.y, c.y + cellSize, c.y);
-		var p = i_ctx.getImageData(centroidX, centroidY, 1, 1).data; 
-		var hex = "#" + ("000000" + rgbToHex(p[0], p[1], p[2])).slice(-6);
+		//get triangle centroid
+		var centroid = new coord(((a.x + b.x + c.x) / 3), ((a.y + b.y + c.y) / 3));
+		//get pixel
+		var p = i_ctx.getImageData(centroid.x, centroid.y, 1, 1).data; 
+		//get color
+		var hex = ("000000" + rgbToHex(p[0], p[1], p[2])).slice(-6);
+		
 		return hex;
-	}
-	
-	function getTriangleCentroidCoordinate(n1, n2, n3)
-	{
-		var result = Math.round((n1+n2+n3)/3);
-		return result;
 	}
 	
 	function rgbToHex(r, g, b) 
